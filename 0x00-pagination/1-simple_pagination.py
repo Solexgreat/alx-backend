5118e737c8ef5c0b44ad400091e388f61164b64f
Solexgreat <solomonGodkid@gmail.com>
2023-02-21 00:35:57 +0100

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,6 +8,7 @@ from typing import List, Tuple
 
 index_range = __import__('0-simple_helper_function').index_range
 
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
