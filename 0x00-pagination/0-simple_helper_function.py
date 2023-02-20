a9650300c8ae1e24e10e4bc3bb83d25f86f5c8f6
Solexgreat <solomonGodkid@gmail.com>
2023-02-20 22:33:57 +0100

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+""" simple helper function """
+
+
 def index_range(page, page_size):
+    """ function return the page and page_size in a
+        tuple """
     i = page_size
-    i = page_size * page
-    page = i - page_size
-    page_size = i
-    result = (page, page)
+    page_size = page_size * page
+    page = page_size - i
+    result = (page, page_size)
     return(result)
