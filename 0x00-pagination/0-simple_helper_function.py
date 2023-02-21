0c43e19abe97bd244f92c344b67137c3137f094b
Solexgreat <solomonGodkid@gmail.com>
2023-02-21 14:35:06 +0100

    simple function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ simple helper function """
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int , page_size: int)-> Tuple[int. int]:
     """ function return the page and page_size in a
         tuple """
     i = page_size
